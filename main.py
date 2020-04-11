8331866594e52e413669af98bf482f60af09c25a
Hansa Chen <hansa.chen@gmail.com>
2020-04-11 01:04:54 -0700

    Changed input and added Regex

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ import os
 import csv
 import requests
 import praw
+import sys
 import praw.models
 import datetime as dt
 
 # setup pushshift API (Input time HERE)
-after = int(dt.datetime(2020, 3, 28).timestamp())
-before = int(dt.datetime(2020, 3, 29).timestamp())
-score = 2
-numberOfComments = 15
+month_after = int(sys.argv[1])
+day_after = int(sys.argv[2])
+month_before = int(sys.argv[3])
+day_before = int(sys.argv[4])
+
+after = int(dt.datetime(2020,month_after,day_after).timestamp())
+before = int(dt.datetime(2020,month_before, day_before).timestamp())
+score = 1
+numberOfComments = 20
 subname = "coronavirus"
 subCount = 0
 subStats = {}
@@ -54,17 +60,18 @@ def collectSubData(subm):
         # print(comment_text)
         if (comment_score < 1):
             continue
-        subData.append((comment_id, comment_text,title,created,permalink))
+        comment_text_updated = comment_text.replace('\r', '').replace('\n', '')
+        subData.append((title,created,permalink, comment_id, comment_text_updated))
         subStats[comment_id] = subData
         subData = list()
 
 #Add to Excel file as output
 def updateSubs_file():
     upload_count = 0
-    file = "data.csv"
+    file ="data/" + str(month_after) + "-" + str(day_after) +".csv"
     with open(file, 'w', newline='', encoding='utf-8') as file:
         a = csv.writer(file, delimiter=',')
-        headers = ["Comment ID","Comment","Title","Publish Date","Permalink"]
+        headers = ["Title","Publish Date","Permalink","Comment ID","Comment"]
         a.writerow(headers)
         for sub in subStats:
             a.writerow(subStats[sub][0])
