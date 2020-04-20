c53bf5c09c3237f1c8b5f504d9bf5a0ec275ac57
Hansa Chen <hansa.chen@gmail.com>
2020-04-19 17:12:41 -0700

    Uploaded Data

--- main.py
+++ main.py
@@ -6,20 +6,15 @@ import praw
 import sys
 import praw.models
 import datetime as dt
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 # setup pushshift API (Input time HERE)
-month_after = int(sys.argv[1])
-day_after = int(sys.argv[2])
-month_before = int(sys.argv[3])
-day_before = int(sys.argv[4])
+month = int(sys.argv[1])
+day = int(sys.argv[2])
 
-after = int(dt.datetime(2020,month_after,day_after).timestamp())
-before = int(dt.datetime(2020,month_before, day_before).timestamp())
-score = 1
 numberOfComments = 20
 subname = "coronavirus"
-subCount = 0
-subStats = {}
+analyzer = SentimentIntensityAnalyzer()
 
 # setup praw API
 keys = {}
@@ -31,65 +26,88 @@ username = keys['username']
 userpwd = keys['userpwd']
 reddit = praw.Reddit(client_id=client, client_secret=secret, password=userpwd, username=username, user_agent='fetch_script')
 
-# Get Posts from Pushshift API
-def getPushshiftData(after, before, sub, score):
-    url = 'https://api.pushshift.io/reddit/search/submission/?&size=1000&score=>'+str(score)+'&after='+str(after)+'&before='+str(before)+'&subreddit='+str(sub)
-    r = requests.get(url)
-    data = json.loads(r.text)
-    return data['data']
 
-# Get comments from post
-def collectSubData(subm):
-    subData = list()
-    sub_id = subm['id']
-    title = subm['title']
-    created = dt.datetime.fromtimestamp(subm['created_utc'])
-    permalink = subm['permalink']
+while month <= 3 and day <= 31:
+    # Get Posts from Pushshift API
+    def getPushshiftData(after, before, sub):
+        url = 'https://api.pushshift.io/reddit/search/submission/?&size=1000&after='+str(after)+'&before='+str(before)+'&subreddit='+str(sub)
+        r = requests.get(url)
+        data = json.loads(r.text)
+        return data['data']
 
-    #praw stuff
-    submission = reddit.submission(id=sub_id)
-    submission.comment_sort = 'best'
-    submission.comment_limit = numberOfComments
-    submission.comments.replace_more(limit=0)
-    comments = submission.comments.list()
-    for top_level_comment in comments:
-        comment_id = top_level_comment.id
-        comment_text = top_level_comment.body
-        comment_score = top_level_comment.score
-        # print(comment_id)
-        # print(comment_text)
-        if (comment_score < 1):
-            continue
-        comment_text_updated = comment_text.replace('\r', '').replace('\n', '')
-        subData.append((title,created,permalink, comment_id, comment_text_updated))
-        subStats[comment_id] = subData
+    # Get comments from post
+    def collectSubData(subm):
         subData = list()
+        sub_id = subm['id']
+        title = subm['title']
+        created = dt.datetime.fromtimestamp(subm['created_utc'])
+        permalink = subm['permalink']
 
-#Add to Excel file as output
-def updateSubs_file():
-    upload_count = 0
-    file ="data/" + str(month_after) + "-" + str(day_after) +".csv"
-    with open(file, 'w', newline='', encoding='utf-8') as file:
-        a = csv.writer(file, delimiter=',')
-        headers = ["Title","Publish Date","Permalink","Comment ID","Comment"]
-        a.writerow(headers)
-        for sub in subStats:
-            a.writerow(subStats[sub][0])
-            upload_count+=1
+        #praw stuff
+        submission = reddit.submission(id=sub_id)
+        if (submission.score >= 20):
+            count = 0
+            title_vs = analyzer.polarity_scores(submission.title)['compound']
+            submission.comment_sort = 'best'
+            submission.comments.replace_more(limit=0)
+            comments = submission.comments.list()
+            nc = numberOfComments
+            if (len(comments) <= numberOfComments):
+                nc = len(comments)
+            while count < nc:
+                top_level_comment = comments[count]
+                comment_id = top_level_comment.id
+                comment_text = top_level_comment.body
+                comment_score = top_level_comment.score
+                if (comment_score < 1):
+                    count = count+1
+                    continue
+                comment_text_updated = comment_text.replace('\r', '').replace('\n', '')
+                comment_vs = analyzer.polarity_scores(comment_text_updated)['compound']
+                subData.append((comment_id,comment_vs,title_vs,comment_text_updated,title,created,permalink))
+                subStats[comment_id] = subData
+                subData = list()
+                count = count+1
 
-def notify(title, text):
-    os.system("""
-              osascript -e 'display notification "{}" with title "{}"'
-              """.format(text, title))
+    #Add to Excel file as output
+    def updateSubs_file():
+        upload_count = 0
+        file ="data/" + str(month) + "-" + str(day) +".csv"
+        with open(file, 'w', newline='', encoding='utf-8') as file:
+            a = csv.writer(file, delimiter=',')
+            headers = ["Comment ID","Comment_VS","Title_VS", "Comment","Title","Publish Date","Permalink"]
+            a.writerow(headers)
+            for sub in subStats:
+                a.writerow(subStats[sub][0])
+                upload_count+=1
 
-data = getPushshiftData(after, before, subname, score)
-while len(data) > 0:
-    for submission in data:
-        collectSubData(submission)
-        subCount+=1
-    after = data[-1]['created_utc']
-    data = getPushshiftData(after, before, subname, score)
-print(str(len(subStats)) + " submissions have added to list")
+    def notify(title, text):
+        os.system("""
+                  osascript -e 'display notification "{}" with title "{}"'
+                  """.format(text, title))
 
-updateSubs_file()
-notify("Done", "Finished " + str(len(subStats)))
+    after = int(dt.datetime(2020,month,day).timestamp())
+    before = int(dt.datetime(2020,month, day+1).timestamp())
+    subCount = 0
+    subStats = {}
+
+    data = getPushshiftData(after, before, subname)
+    while len(data) > 0:
+        for submission in data:
+            collectSubData(submission)
+            subCount+=1
+        after = data[-1]['created_utc']
+        data = getPushshiftData(after, before, subname)
+    print(str(len(subStats)) + " submissions have added to list")
+
+    updateSubs_file()
+    notify("Done", "Finished " + str(month) + " " + str(day) + " " + str(len(subStats)))
+
+    if (month == 1 and day == 31):
+        month = 2
+        day = 1
+    elif (month == 2 and day == 28):
+        month = 3
+        day = 1
+    else:
+        day = day+1
